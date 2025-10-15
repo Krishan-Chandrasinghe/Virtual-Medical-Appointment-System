b7000034986b8e2b72db689f5434128db9770a20
Krishan-Malinda <krishanmalinda99@gmail.com>
2025-10-15 23:21:59 +0530

    Fixed Logout Error

--- Frontend/src/components/NavBar.jsx
+++ Frontend/src/components/NavBar.jsx
@@ -1,21 +1,27 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
+import { Link } from 'react-router-dom'
 import UniLogo from '../assets/UniLogo.webp';
 import { useAuthContext } from '../hooks/useAuthContext';
+import api from '../api/apiConfig';
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
   const { user, dispatch } = useAuthContext();
 
+  const handleLogout = async () => {
+    dispatch({ type: 'LOGOUT' });
+    await api.post('users/logout');
+  }
+
   return (
     <nav className="bg-white shadow-lg border-b border-gray-200 sticky top-0 z-50">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16">
-          {/* Logo and Brand Name */}
           <div className="flex items-center">
             <div className="flex-shrink-0 flex items-center">
               <img
                 className="h-8 w-8"
-                src={UniLogo} // Replace with your logo path
+                src={UniLogo}
                 alt="Your Company"
               />
               <span className="ml-2 text-xl font-bold text-[#3a1031]">
@@ -48,12 +54,20 @@ const Navbar = () => {
 
           {/* Desktop Menu - Right Side */}
           {
-            user && 
+            user &&
             <div className="hidden md:flex items-center space-x-4">
               <h4 className='text-[#3a1031] text-sm font-bold'>{user.firstName}</h4>
-              <button onClick={() => dispatch({ type: 'LOGOUT' })} className="border border-red-600 hover:bg-red-700 text-gray-700 hover:text-white px-2 py-1 rounded-md text-sm font-medium transition duration-300">
-                Logout
+              <button onClick={handleLogout} className="border border-red-600 hover:bg-red-700 text-gray-700 hover:text-white px-2 py-1 rounded-md text-sm font-medium transition duration-300">
+                Log Out
               </button>
+            </div>
+          }
+          {
+            !user &&
+            <div className="hidden md:flex items-center space-x-4">
+              <Link to={'/login'} className="border border-[#3a1031] hover:bg-[#3a1031] text-[#3a1031] hover:text-white px-2 py-1 rounded-md text-sm font-medium transition duration-300">
+                Log in
+              </Link>
             </div>
           }
 
@@ -122,11 +136,22 @@ const Navbar = () => {
             Contact Us
           </a>
           <div className="border-t border-gray-200 pt-4 pb-3">
-            <div className="mt-3 space-y-1">
-              <button className="block w-full text-left px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-red-600 hover:bg-gray-50">
-                Log out
-              </button>
-            </div>
+            {
+              user &&
+              <div className="mt-3 space-y-1">
+                <button onClick={handleLogout} className="block w-full text-left px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-red-600 hover:bg-gray-50">
+                  Log out
+                </button>
+              </div>
+            }
+            {
+              !user &&
+              <div className="mt-3 space-y-1">
+                <Link to={'/login'} className="block w-full text-left px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-[#3a1031] hover:bg-gray-50">
+                  Log in
+                </Link>
+              </div>
+            }
           </div>
         </div>
       </div>
